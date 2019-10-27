93ba59c8bd9f34eaa0da63c57e9cef1737382979
wanzhou <wanzhou0310@163.com>
2019-10-28 00:10:28 +0800

    20191028

--- src/main/java/com/java/leetcode/StackandQueue_06/LeetCode_111.java
+++ src/main/java/com/java/leetcode/StackandQueue_06/LeetCode_111.java
@@ -17,14 +17,12 @@ public class LeetCode_111 {
             }
 
             if (!(root.left == null && root.right == null) && !(root.left != null && root.right != null)) {
-//                if (root.left != null) {
-//                    return minDepth(root.left) + 1;
-//                }
-//                if (root.right != null) {
-//                    return minDepth(root.right) + 1;
-//                }
-
-                return minDepth(root.left) + minDepth(root.right) + 1;
+                if (root.left != null) {
+                    return minDepth(root.left) + 1;
+                }
+                if (root.right != null) {
+                    return minDepth(root.right) + 1;
+                }
             }
 
             return Math.min(minDepth(root.left), minDepth(root.right)) + 1;
